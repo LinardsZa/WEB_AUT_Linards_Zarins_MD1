49b0ba2ce4c846c03d46d74a5f571e31f7f2e39a
LZ <linards.zarins@va.lv>
2025-05-29 19:43:04 +0300

    small fix to md 2

--- cypress/e2e/grid.cy.js
+++ cypress/e2e/grid.cy.js
@@ -1,25 +1,22 @@
 import { GridPage } from "../PObjects/GridPage";
 
-describe("Selectable - Grid scenario", () => {
+describe("Selecting grid values", () => {
   beforeEach(() => {
     GridPage.visit();
     GridPage.gridTab.click();
   });
 
-  it("Select even grid items and validate highlighting", () => {
-    // Click even-numbered items
+  it("Select grid items that are even and validate the ones that are not selected", () => {
     GridPage.getItem("Two").click();
     GridPage.getItem("Four").click();
     GridPage.getItem("Six").click();
     GridPage.getItem("Eight").click();
 
-    // Validate they are selected
     GridPage.getItem("Two").should("have.class", "active");
     GridPage.getItem("Four").should("have.class", "active");
     GridPage.getItem("Six").should("have.class", "active");
     GridPage.getItem("Eight").should("have.class", "active");
 
-    // Validate unselected items
     GridPage.getItem("One").should("not.have.class", "active");
     GridPage.getItem("Three").should("not.have.class", "active");
     GridPage.getItem("Five").should("not.have.class", "active");
